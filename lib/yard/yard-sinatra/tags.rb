b6e0eeac93914d0686d6add2a4b2e561d41c7957
Naveen Narayanan <naveen.narayanan@lookout.com>
2013-11-06 14:29:57 -0800

    Modified format for request tag

--- lib/yard/yard-sinatra/tags.rb
+++ lib/yard/yard-sinatra/tags.rb
@@ -2,8 +2,6 @@
 
 YARD::Tags::Library.define_tag("Request", :request, :with_title_and_text)
 YARD::Tags::Library.define_tag("Response", :response, :with_title_and_text)
-YARD::Tags::Library.define_tag("Response Fields", :response_field, :with_types_and_name)
-YARD::Tags::Library.define_tag("Request Fields", :request_field, :with_types_and_name)
 YARD::Tags::Library.define_tag("Headers", :header, :with_name)
 YARD::Tags::Library.define_tag("Response codes", :response_code, :with_name)
 YARD::Tags::Library.define_tag("Image", :image)
